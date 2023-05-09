08f3545a75a6dfabcf3697a47718e56ae5116f00
g.mangelli <giacomomsidp@gmail.com>
2023-05-09 14:06:23 +0200

    feat(main): reformat connection class

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from repository.connection.connection import CreateConnection
+from repository.connection.connection import Connection
+import logging
 
 
-def main(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    connection1 = CreateConnection
-    cursor = connection1.connection.cursor()
-    # SQL query to create a new table
+def main():
+    opened_connection = Connection.connection
+
+    if not Connection.check_connection(opened_connection):
+        return logging.info("Error in connection")
+
+    cursor = opened_connection.connection.cursor()
     create_table_query = '''CREATE TABLE mobile
             (ID INT PRIMARY KEY     NOT NULL,
             MODEL           TEXT    NOT NULL,
             PRICE         REAL); '''
-    # Execute a command: this creates a new table
+
     cursor.execute(create_table_query)
-    connection1.connection.commit()
-    connection1.close_connection()
+    opened_connection.connection.commit()
+    opened_connection.close_connection()
     print("Table created successfully in PostgresSQL ")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- repository/connection/connection.py
+++ repository/connection/connection.py
@@ -2,23 +2,30 @@ import psycopg2
 import logging
 
 
+class Connection:
+    connection = None
+
+    def __init__(self):
+        self.connection = create_connection()
+
+    @classmethod
+    def check_connection(cls, connection):
+        if connection is not None:
+            return True
+        else:
+            return False
+
+
 def create_connection():
-    pass
+    return psycopg2.connect(user="giulia",
+                            password="password",
+                            host="127.0.0.1",
+                            port="5432",
+                            database="postgres")
 
 
-class CreateConnection:
-    connection = create_connection()
+def close_connection(connection):
+    if connection:
+        connection.close()
+    logging.info('Connection closed')
 
-    @classmethod
-    def create_connection():
-        return psycopg2.connect(user="giulia",
-                                password="password",
-                                host="127.0.0.1",
-                                port="5432",
-                                database="postgres")
-
-    @classmethod
-    def close_connection(connection):
-        if connection:
-            connection.close()
-        logging.info('Connection closed')
